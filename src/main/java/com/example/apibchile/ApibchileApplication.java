e01603de9fe12ffdd24fd958db49093cd9dca4e0
andresbluna <andres.benjamin.luna@gmail.com>
2024-05-19 23:39:50 -0400

    Swagger UI ok

--- src/main/java/com/example/apibchile/ApibchileApplication.java
+++ src/main/java/com/example/apibchile/ApibchileApplication.java
@@ -1,13 +1,30 @@
 package com.example.apibchile;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.info.Contact;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Demo Api Rest BCHILE",
+				version = "v1",
+				description = "Documentación de la API REST BCHILE",
+				contact = @Contact(email = "tu-email@example.com"),
+				license = @License(name = "MIT", url = "https://opensource.org/licenses/MIT")
+		)
+)
+
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ApibchileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApibchileApplication.class, args);
 	}
+
+
 
 }
--- src/main/java/com/example/apibchile/controller/IndicatorController.java
+++ src/main/java/com/example/apibchile/controller/IndicatorController.java
@@ -3,6 +3,9 @@ package com.example.apibchile.controller;
 import com.example.apibchile.model.IndicatorData;
 import com.example.apibchile.model.UfDetail;
 import com.example.apibchile.service.IndicatorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +17,21 @@ public class IndicatorController {
     @Autowired
     private IndicatorService indicatorService;
 
+    @Operation(summary = "Obtiene datos de indicadores", description = "Retorna una lista de datos de indicadores.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Datos de indicadores obtenidos exitosamente."),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor.")
+    })
     @GetMapping("/indicators")
     public IndicatorData getIndicators() {
         return indicatorService.getIndicators();
     }
 
+    @Operation(summary = "Obtiene datos de UF", description = "Retorna datos detallados de UF.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Datos de UF obtenidos exitosamente."),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor.")
+    })
     @GetMapping("/uf")
     public UfDetail getUfData() {
         return indicatorService.getUfData();
